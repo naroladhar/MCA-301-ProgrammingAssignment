96774addda8cf833bc095e5cf2f3dcec177e698b
naroladhar <32682490+naroladhar@users.noreply.github.com>
2018-11-07 02:08:32 +0530

    Update and rename dijikstra(Q2a).cpp to dijkstra(Q2a).cpp

--- dijkstra(Q2a).cpp
+++ dijkstra(Q2a).cpp
@@ -62,8 +62,7 @@ void dijkstra(int graph[V][V], int src)
 		// Update dist[v] only if is not in sptSet, there is an edge from 
 		// u to v, and total weight of path from src to v through u is 
 		// smaller than current value of dist[v] 
-		if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
-									&& dist[u]+graph[u][v] < dist[v]) 
+		if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u]+graph[u][v] < dist[v]) 
 			dist[v] = dist[u] + graph[u][v]; 
 	} 
 
@@ -76,15 +75,15 @@ int main()
 { 
 /* Let us create the example graph discussed above */
 int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0}, 
-					{4, 0, 8, 0, 0, 0, 0, 11, 0}, 
-					{0, 8, 0, 7, 0, 4, 0, 0, 2}, 
-					{0, 0, 7, 0, 9, 14, 0, 0, 0}, 
-					{0, 0, 0, 9, 0, 10, 0, 0, 0}, 
-					{0, 0, 4, 14, 10, 0, 2, 0, 0}, 
-					{0, 0, 0, 0, 0, 2, 0, 1, 6}, 
-					{8, 11, 0, 0, 0, 0, 1, 0, 7}, 
-					{0, 0, 2, 0, 0, 0, 6, 7, 0} 
-					}; 
+		   {4, 0, 8, 0, 0, 0, 0, 11, 0}, 
+		   {8, 0, 7, 0, 4, 0, 0, 2}, 
+		   {0, 0, 7, 0, 9, 14, 0, 0, 0}, 
+		   {0, 0, 0, 9, 0, 10, 0, 0, 0}, 
+		   {0, 0, 4, 14, 10, 0, 2, 0, 0}, 
+		   {0, 0, 0, 0, 0, 2, 0, 1, 6}, 
+		   {8, 11, 0, 0, 0, 0, 1, 0, 7}, 
+		   {0, 0, 2, 0, 0, 0, 6, 7, 0} 
+		  }; 
 
 	dijkstra(graph, 0); 
 
